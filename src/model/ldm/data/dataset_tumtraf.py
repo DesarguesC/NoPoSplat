e85df4694e81d164eedb108a453f35c3be0e7422
DesarguesC <425593160@qq.com>
2025-01-14 20:35:31 +0800

    Img MISSing

--- src/model/ldm/data/dataset_tumtraf.py
+++ src/model/ldm/data/dataset_tumtraf.py
@@ -11,6 +11,7 @@ from typing import Optional
 from random import randint
 import numpy as np
 from utils import positional_encode, camera2ray
+from tqdm import tqdm
 
 
 K_veh = np.array([[2730.3754, 0., 960.], [0., 2730.3754, 600.], [0., 0., 1.]]) # -> create camera ray
@@ -67,11 +68,11 @@ def capture_point_cloud_render(pcd, K, trans, shape):
 
 
 
-def isContinue(id_1: str, id_2: str) -> bool:
+def isContinue(id_1, id_2) -> bool:
     # id_1 > id_2
-    u_1, u_2 = id_1.split('_'), id_2.split('_')
+    u_1, u_2 = id_1, id_2
     assert len(u_1) == 2 and len(u_2) == 2, f'u_1 = {u_1}, u2 == {u_2}'
-    return (u_1[0] == u_2[0] and str(int(u_1[1]) + 1) == u_2[1]) or (str(int(u_1[0]) + 1) == u_2[0])
+    return abs(int(u_1[0]) - int(u_2[0])) <= 1
 
 def IsInOneVideo(id_list, idx_1, idx_2) -> bool:
     assert idx_2 > idx_1
@@ -87,7 +88,7 @@ def getStartEndList(id_list, frame):
     se_list = []
     pdb.set_trace()
     for i in range(len(id_list)-frame):
-        if IsInOneVideo(i, i + frame - 1):
+        if IsInOneVideo(id_list, i, i + frame - 1):
             se_list.append((i, i + frame))
     # TODO: convert to one line expression after debugging
     return se_list
@@ -102,6 +103,7 @@ class TumTrafDataset():
         self.val = os.path.join(root_path, 'val')
         self.resolution = resolution
         H, W = self.resolution
+        self.res = (H, W)
         self.downsample = downsample
         self.downsampler = partial(F.interpolate, size=(H, W), mode='bilinear')
 
@@ -113,33 +115,43 @@ class TumTrafDataset():
         self.vehicle_path = os.path.join(self.use, 'images', 'vehicle_camera_basler_16mm')
 
         cfg_base_path = os.path.join(self.train, 'labels_point_clouds', 's110_lidar_ouster_south_and_vehicle_lidar_robosense_registered')
-        train_list = os.listdir(sorted(cfg_base_path))
+        train_list = sorted(os.listdir(cfg_base_path))
         # TODO: need 'src'&'dst'&'transform'&'src img path'&'dst img path'
         self.data_src = []
         self.id_list = []
-        for file in train_list:
+
+        for file in tqdm(train_list):
             if file.endswith('.json'):
-                with open(os.path.join(file), 'r') as f:
+                with open(os.path.join(cfg_base_path, file), 'r') as f:
                     u = json.load(f)
+
                 key = list(u['openlabel']['frames'])[0]
                 file_dict = u['openlabel']['frames'][key]['frame_properties']
                 img_list = sorted(file_dict['image_file_names'])
+                # pdb.set_trace()
                 tmp = img_list[0].split('_')
                 id_num = f'{tmp[0]}_{tmp[1]}'
-                self.id_list.append(id_num)
-                self.data_src.append(
-                        {
-                            id_num : {
-                            'south1': os.path.join(self.south1_path, img_list[-1]),
-                            'south2': os.path.join(self.south2_path, img_list[-2]),
-                            'vehicle': os.path.join(self.vehicle_path, img_list[1]),
+
+                south1 = os.path.join(self.south1_path, img_list[-1])
+                south2 = os.path.join(self.south2_path, img_list[-2])
+                vehicle = os.path.join(self.vehicle_path, img_list[1])
+
+                pdb.set_trace()
+
+                if os.path.isfile(south1) and os.path.isfile(south2) and os.path.isfile(vehicle):
+                    # TODO: why considerable amount of images in config are missing ?
+                    self.id_list.append((tmp[0], tmp[1]))
+                    self.data_src.append({
+                            'south1': south1,
+                            'south2': south2,
+                            'vehicle': vehicle,
                             'transform': np.array(
                                 file_dict['transforms']['vehicle_lidar_robosense_to_s110_lidar_ouster_south'][
                                     'transform_src_to_dst']['matrix4x4'], dtype=np.float32)
-                        }
-                    }
-                )
-        self.id_list.sort()
+                    })
+        
+        pdb.set_trace()
+        # self.id_list.sort()
         self.start_end_list = getStartEndList(self.id_list, self.frame)
 
     def convert_dict(self, item, use_south1=True):
@@ -148,7 +160,10 @@ class TumTrafDataset():
             'vehicle': item['vehicle'], # g.t.
             'intrinsic': item['inf-intrinsic'],
             'video': item['south1' if use_south1 else 'south2'],
-            'ray': item['transform'],
+            'ray': {
+                'pos': item['ray_pos'],
+                'dir': item['ray_dir']
+            },
         }
         # 'video', ray'
 
@@ -160,19 +175,24 @@ class TumTrafDataset():
         # id_list = self.id_list[start:end]
         dict_list = self.data_src[start:end]
         item_dict = {
-            'south1': [], 'south2': [], 'vehicle': [], 'transform': [], 'inf-intrinsic': []
+            'south1': [], 'south2': [], 'vehicle': [], 'ray_pos': [], 'ray_dir': [], 'inf-intrinsic': []
         }
+        pdb.set_trace()
         for u in dict_list:
-            item_dict['south1'].append(img2tensor(cv2.imread(u['south1'])[None,:]))
-            item_dict['south2'].append(img2tensor(cv2.imread(u['south2'])[None,:]))
-            item_dict['vehicle'].append(img2tensor(cv2.imread(u['vehicle'])[None,:]))
-            item_dict['transform'].append(camera2ray(u['transform'], K_veh, self.resolution)[None,:])
+            item_dict['south1'].append(img2tensor(cv2.imread(u['south1']))[None,:]) # [1 3 h w]
+            item_dict['south2'].append(img2tensor(cv2.imread(u['south2']))[None,:]) # [1 3 h w]
+            item_dict['vehicle'].append(img2tensor(cv2.imread(u['vehicle']))[None,:]) # [1 3 h w]
+            ray = camera2ray(u['transform'], K_veh, self.res)[None,:]
+            item_dict['ray_pos'].append(torch.tensor(ray[0], dtype=torch.float32))
+            item_dict['ray_dir'].append(torch.tensor(ray[1], dtype=torch.float32))
             item_dict['inf-intrinsic'].append(K_inf)
 
-        item_dict['south1'] = self.downsampler(torch.cat(item_dict['south1'], dim=0))[None, :]
-        item_dict['south2'] = self.downsampler(torch.cat(item_dict['south2'], dim=0))[None, :]
-        item_dict['vehicle'] = self.downsampler(torch.cat(item_dict['vehicle'], dim=0))[None, :]
-        item_dict['transform'] = self.downsampler(torch.cat(item_dict['transform'], dim=0))[None, :]
+        item_dict['south1'] = self.downsampler(torch.cat(item_dict['south1'], dim=0))[None, :] # [1 f 3 h w]
+        item_dict['south2'] = self.downsampler(torch.cat(item_dict['south2'], dim=0))[None, :] # [1 f 3 h w]
+        item_dict['vehicle'] = self.downsampler(torch.cat(item_dict['vehicle'], dim=0))[None, :] # [1 f 3 h w]
+        item_dict['ray_pos'] = torch.cat(item_dict['ray_pos'], dim=0)[None, :] # [1 f 3 h w]
+        item_dict['ray_dir'] = torch.cat(item_dict['ray_dir'], dim=0)[None, :] # [1 f 3 h w]
+
 
         return self.convert_dict(item_dict)
 
@@ -182,6 +202,6 @@ class TumTrafDataset():
 
 if __name__ == '__main__':
     dataset = TumTrafDataset(root_path='../../../../../download/tumtraf_v2x_cooperative_perception_dataset', frame=16)
-    pdb.set_trace()
+    # pdb.set_trace()
     item = dataset[15]
     print(item.keys())
