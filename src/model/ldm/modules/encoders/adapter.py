7775e703a056267a433c3511bec42f98c8e5d9ca
DesarguesC <425593160@qq.com>
2025-01-27 10:38:26 +0800

    adapters

--- src/model/ldm/modules/encoders/adapter.py
+++ src/model/ldm/modules/encoders/adapter.py
@@ -1,6 +1,11 @@
 import torch
 import torch.nn as nn
+import numpy as np
+from einops import repeat, rearrange
 from collections import OrderedDict
+from torch.nn import init
+import pdb
+from timm.models.layers import DropPath, to_2tuple
 
 
 def conv_nd(dims, *args, **kwargs):
@@ -95,9 +100,13 @@ class ResnetBlock(nn.Module):
 
 
 class Adapter(nn.Module):
-    def __init__(self, channels=[320, 640, 1280, 1280], nums_rb=3, cin=64, ksize=3, sk=False, use_conv=True):
+    def __init__(self, channels=[640, 1280, 1280], nums_rb=3, frame=20, patches=320, ksize=1, sk=True, use_conv=False,
+                 train_mode=True):
+        # patches: p * n = (H//p_size) * (W//p_size) * 2 * n // 8
         super(Adapter, self).__init__()
-        self.unshuffle = nn.PixelUnshuffle(8)     # amend ?
+        self.frame = frame
+        self.shuffle = nn.PixelShuffle(2)  # amend ?
+        self.unshuffle = nn.PixelUnshuffle(4)
         self.channels = channels
         self.nums_rb = nums_rb
         self.body = []
@@ -110,22 +119,40 @@ class Adapter(nn.Module):
                     self.body.append(
                         ResnetBlock(channels[i], channels[i], down=False, ksize=ksize, sk=sk, use_conv=use_conv))
         self.body = nn.ModuleList(self.body)
-        self.conv_in = nn.Conv2d(cin, channels[0], 3, 1, 1)
+        self.conv_in = nn.Conv2d(patches, channels[0], 3, 1, 1)
+        if train_mode:
+            self.initialize_weights()
 
     def forward(self, x):
-        # unshuffle
+        pdb.set_trace()
+        x = rearrange(x, 'n b f p e -> b f (p n) e')  # 或者n和f放一起？
         x = self.unshuffle(x)
         # extract features
         features = []
-        x = self.conv_in(x)
+
+        x = self.conv_in(x)  # # torch.Size([2, 320, 1280, 72])
         for i in range(len(self.channels)):
             for j in range(self.nums_rb):
-                idx = i * self.nums_rb\
+                idx = i * self.nums_rb \
                       + j
                 x = self.body[idx](x)
             features.append(x)
 
         return features
+
+    def initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                init.kaiming_uniform_(m.weight, nonlinearity='relu')
+                if m.bias is not None:
+                    init.zeros_(m.bias)
+            elif isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    init.zeros_(m.bias)
+            elif isinstance(m, nn.BatchNorm2d):
+                init.ones_(m.weight)
+                init.zeros_(m.bias)
 
 
 class LayerNorm(nn.LayerNorm):
@@ -233,27 +260,134 @@ class extractor(nn.Module):
         return x
 
 
+class PatchEmbed(nn.Module):
+    """ Image to Patch Embedding
+    """
+
+    def __init__(self, img_size=256, patch_size=16, kernel_size=1, in_chans=6, embed_dim=576):
+        # in_chans
+        # 2 ray maps concatenated at channel
+        super().__init__()
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
+        num_patches = (img_size[1] // patch_size[1]) * (img_size[0] // patch_size[0])
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.num_patches = num_patches
+        self.tubelet_size = kernel_size
+
+        self.proj = nn.Conv3d(
+            in_chans, embed_dim,
+            kernel_size=(kernel_size, patch_size[0], patch_size[1]),
+            stride=(kernel_size, patch_size[0], patch_size[1])
+        )
+
+    def forward(self, x):
+        pdb.set_trace()
+        x = self.proj(x)
+        return x
+
+
 class Adapter_light(nn.Module):
-    def __init__(self, channels=[320, 640, 1280, 1280], nums_rb=3, cin=64):
+    def __init__(self, channels=[640, 1280, 1280], nums_rb=3, frame=20, patches=1200, img_size=256, patch_size=4,
+                 embed_dim=576):
         super(Adapter_light, self).__init__()
-        self.unshuffle = nn.PixelUnshuffle(8)
+        self.frame = frame
+        self.embedding_proj = PatchEmbed(img_size=img_size, patch_size=patch_size, embed_dim=embed_dim)
+        self.shuffle = nn.PixelShuffle(4)
         self.channels = channels
         self.nums_rb = nums_rb
         self.body = []
         for i in range(len(channels)):
             if i == 0:
-                self.body.append(extractor(in_c=cin, inter_c=channels[i]//4, out_c=channels[i], nums_rb=nums_rb, down=False))
+                # TODO: Here, the detail of the extractor
+                self.body.append(
+                    extractor(in_c=patches, inter_c=channels[i] // 4, out_c=channels[i], nums_rb=nums_rb, down=False))
             else:
-                self.body.append(extractor(in_c=channels[i-1], inter_c=channels[i]//4, out_c=channels[i], nums_rb=nums_rb, down=True))
+                self.body.append(
+                    extractor(in_c=channels[i - 1], inter_c=channels[i] // 4, out_c=channels[i], nums_rb=nums_rb,
+                              down=True))
         self.body = nn.ModuleList(self.body)
+        # self.conv_in = nn.Conv2d(patches, channels[0], 3, 1, 1)
 
     def forward(self, x):
+        x = rearrange(x, 'n b f c h w -> b c (n f) h w')
+        pdb.set_trace()
+        x = self.embedding_proj(x)  # [b, c, f, h, w] -> [b, e, f, h1, w1]
+        pdb.set_trace()
+        x = rearrange(x, 'b e f h w -> b (e f) h w')  # 或者n和f放一起？
+        x = self.shuffle(x)
         # unshuffle
-        x = self.unshuffle(x)
+        # x = rearrange(self.unshuffle(x), 'b f h w -> b p h w')
         # extract features
         features = []
+
+        # x = self.conv_in(x)
         for i in range(len(self.channels)):
             x = self.body[i](x)
             features.append(x)
 
         return features
+
+
+def main1():
+    dec_feat = torch.randn((10, 2, 20, 256 ** 2 // 256 * 2, 576))  # [n, b, f, p, e] -> [b f (p n) e]
+    dec_feat = dec_feat.to('cuda')
+    pdb.set_trace()
+
+    adapter_dec = Adapter(
+        frame=20,
+        patches=320,
+        channels=[320, 160, 80],
+        nums_rb=20,
+        # ksize=1,
+        # sk=True,
+        # use_conv=False
+    ).to('cuda')
+    feature_ad = adapter_dec(dec_feat)
+    pdb.set_trace()
+    # Adapter: torch.Size([2, 640, 1280, 72]), torch.Size([2, 1280, 640, 36]), torch.Size([2, 1280, 320, 18])
+    print(feature_ad[0].shape)
+
+
+def main2():
+    # 256 × 256
+    pos = torch.ones((256, 256, 3)) * 3 / 255.
+    dir = torch.ones((256, 256, 3)) * 3 / 20.
+
+    pos = pos.to('cuda')
+    dir = dir.to('cuda')
+    pdb.set_trace()
+
+    adapter_dec = Adapter_light(
+        frame=20,
+        patches=320,  # n * f * 3
+        channels=[320, 160, 80],
+        nums_rb=20,
+        # ksize=1,
+        # sk=True,
+        # use_conv=False
+    ).to('cuda')
+    # [2 256, 256, 3]
+    input = torch.cat([pos, dir], dim=-1)  # [256 256 3]
+    input = input[None, :]
+    input = repeat(input, '1 ... -> u ...', u=10 * 2 * 20)  # n * b * f
+    input = rearrange(input, '(n b f) h w c -> n b f c h w', n=10, b=2).to('cuda')
+    pdb.set_trace()
+    # [n b f c H W]
+    feature_ad = adapter_dec(input)
+
+    pdb.set_trace()
+    # Adapter_light: torch.Size([2, 640, 256, 256]), torch.Size([2, 1280, 128, 128]), torch.Size([2, 1280, 64, 64])
+    print(feature_ad[0].shape)
+
+
+# TODO: embeddings -> (H, W) ~ diffusion
+class Adapter_Decoder(nn.Module):
+    def __init__(self):
+        ...
+
+
+if __name__ == '__main__':
+    main1()
+
