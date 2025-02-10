169561691cc392f31a688084aeafe27fb935a375
DesarguesC <425593160@qq.com>
2025-02-10 10:11:16 +0800

    debug

--- src/model/decoder/__init__.py
+++ src/model/decoder/__init__.py
@@ -1,12 +1,23 @@
 from .decoder import Decoder
-from .decoder_splatting_cuda import DecoderSplattingCUDA, DecoderSplattingCUDACfg
+from dataclasses import dataclass
+from typing import Literal
 
-DECODERS = {
-    "splatting_cuda": DecoderSplattingCUDA,
-}
+@dataclass
+class DecoderSplattingCUDACfg:
+    name: Literal["splatting_cuda"]
+    background_color: list[float]
+    make_scale_invariant: bool
+
+try:
+    from .decoder_splatting_cuda import DecoderSplattingCUDA
+    DECODERS = {
+        "splatting_cuda": DecoderSplattingCUDA,
+    }
+
+except Exception as err:
+    print(err)
+
 
 DecoderCfg = DecoderSplattingCUDACfg
-
-
 def get_decoder(decoder_cfg: DecoderCfg) -> Decoder:
     return DECODERS[decoder_cfg.name](decoder_cfg)
