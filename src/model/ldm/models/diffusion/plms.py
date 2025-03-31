5c7ce17045c2e478f9bfd974d51389fd1ef23ff1
DesarguesC <425593160@qq.com>
2025-03-31 14:14:29 +0800

    debug

--- src/model/ldm/models/diffusion/plms.py
+++ src/model/ldm/models/diffusion/plms.py
@@ -161,7 +161,7 @@ class PLMSSampler(object):
                 assert x0 is not None
                 img_orig = self.model.q_sample(x0, ts)  # TODO: deterministic forward pass?
                 img = img_orig * mask + (1. - mask) * img
-            pdb.set_trace()
+            # pdb.set_trace()
             outs = self.p_sample_plms(img, cond, ts, index=index, use_original_steps=ddim_use_original_steps,
                                       quantize_denoised=quantize_denoised, temperature=temperature,
                                       noise_dropout=noise_dropout, score_corrector=score_corrector,
